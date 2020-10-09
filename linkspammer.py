bfb90deee7299d5c8149ebc20e20aa0111bea617
rustywolf021 <46093036+rustywolf021@users.noreply.github.com>
2020-10-09 21:15:56 +1300

    Update linkspammer.py

--- linkspammer.py
+++ linkspammer.py
@@ -16,30 +16,19 @@ headers = {
     'user-agent': useragent,
 }
 
-#filepath = 'Iliad.txt'
-#with open(filepath) as fp:
-#   line = fp.readline()
-#   cnt = 1
-#   while line:
-#       print("Line {}: {}".format(cnt, line.strip()))
-#       line = fp.readline()
-#       cnt += 1
-
-
 while loop > 0:
-	f = open("proxies.txt", "r")
-	prox = f.readline()
-	proxy = prox.rstrip("\n")
-	print(proxy)
-	proxies = {
-		'http': 'http://'+proxy,
-		'https': 'https://'+proxy,
-	}
-	print(proxies)
-	response = requests.get(url, headers=headers, proxies=proxies)
-	print("Went to", url)
-	print(url, "= True", "Looped:", loop, "times", "Used proxie:", proxy) #DEBUG LINE
-	loop = loop - 1
+    f = open("proxies.txt", "r")
+    prox = f.readline()
+    proxy = prox.rstrip("\n")
+    proxies = {
+        'http': 'http://'+proxy,
+        'https': 'https://'+proxy,
+    }
+    print(proxies)
+    response = requests.get(url, headers=headers, proxies=proxies)
+    print("Went to", url)
+    print(url, "= True", "Looped:", loop, "times", "Used proxie:", proxy) #DEBUG LINE
+    loop = loop - 1
 
 
 time.sleep(5)
